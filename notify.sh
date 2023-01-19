d7e04f2f2bea608aaeb998ba51c8f748c080fd0b
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-01-19 01:24:58 -0800

    Remove old script

--- notify.sh
+++
@@ -1,6 +0,0 @@
-curl --header 'Access-Token: $PUSHBULLET_TOKEN' \
-     --header 'Content-Type: application/json' \
-     --data-binary "{\"body\":\"FOUND $1 BEFORE $2\",\"title\":\"NEW APPT\",\"type\":\"note\"}" \
-     --request POST \
-     https://api.pushbullet.com/v2/pushes
-
