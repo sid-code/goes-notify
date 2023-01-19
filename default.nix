099386a12ad1635a8a01956f81dd9dcf727d6fe1
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-01-18 22:10:09 -0800

    make default.nix a module, move old stuff to goes-notify.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,52 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs, lib, config, ... }:
+
+with lib;
+
 let
-  my-python-packages = p: with p; [
-    requests
-  ];
-  my-python = pkgs.python3.withPackages my-python-packages;
+  goes-notify = import ./goes-notify { inherit pkgs; };
+  cfg = config.services.fusuma;
+in {
+  options.services.goes-notify = {
+    enable = mkEnableOption "Enable goes-notify service";
 
-  notifyScript = import ./notify-script { inherit pkgs; };
+    package = mkOption {
+      type = types.package;
+      default = goes-notify;
+      defaultText = "<goes-notify>";
+      description = "Set version of goes-notify package to use.";
+    };
 
-  watcher = pkgs.concatTextFile {
-    name = "goes-watcher";
-    files = [ ./goes-notify.py ];
+    enrollmentLocationId = mkOption {
+      type = types.int;
+      default = 5180;
+      description = "The Trusted Traveler Program enrollment location";
+    };
+
+    appointmentDate = mkOption {
+      type = types.string;
+      default = "April 22, 2099";
+      description = "The date of your appointment";
+    };
   };
 
-  config = pkgs.concatTextFile {
-    name = "goes-config";
-    files = [ ./config.json ];
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ]; # if user should have the command available as well
+
+    systemd.services.fusuma = {
+      description = "goes-notify watcher";
+
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ]; # if networking is needed
+
+      restartIfChanged = true; # set to false, if restarting is problematic
+
+      serviceConfig = {
+        DynamicUser = true;
+        ExecStart = "${cfg.package}/bin/goes-notify";
+        Restart = "on-failure";
+      };
+    };
   };
-in
-pkgs.writeShellScriptBin "goes-notify" ''
-set -e
-${my-python}/bin/python ${watcher} "$@"
-''
+
+  meta.maintainers = with lib.maintainers; [  ];
+}
---
+++ goes-notify.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python-packages = p: with p; [
+    requests
+  ];
+  my-python = pkgs.python3.withPackages my-python-packages;
+
+  notifyScript = import ./notify-script { inherit pkgs; };
+
+  watcher = pkgs.concatTextFile {
+    name = "goes-watcher";
+    files = [ ./goes-notify.py ];
+  };
+
+  config = pkgs.concatTextFile {
+    name = "goes-config";
+    files = [ ./config.json ];
+  };
+in
+pkgs.writeShellScriptBin "goes-notify" ''
+set -e
+${my-python}/bin/python ${watcher} "$@"
+''
