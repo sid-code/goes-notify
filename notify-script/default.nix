1eb96776449c081d8a2adcb51cc454af27c566b4
Sid Kulkarni <skulk@google.com>
2023-01-20 09:40:56 -0800

    pushbullet: push to channel

--- notify-script/default.nix
+++ notify-script/default.nix
@@ -1,10 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
-let pushbulletToken = pkgs.lib.removeSuffix "\n" (builtins.readFile ./pushbullet-token);
+let
+  pushbulletToken = pkgs.lib.removeSuffix "\n" (builtins.readFile ./pushbullet-token);
+  pushbulletChannel = "goes-notifications-lax";
 in
 pkgs.writeShellScriptBin "notify.sh" ''
 ${pkgs.curl}/bin/curl --header 'Access-Token: ${pushbulletToken}' \
      --header 'Content-Type: application/json' \
-     --data-binary "{\"body\":\"FOUND $1 BEFORE $2\",\"title\":\"NEW APPT\",\"type\":\"note\"}" \
+     --data-binary "{\"body\":\"FOUND $1 BEFORE $2\",\"title\":\"NEW APPT\",\"type\":\"note\",\"channel_tag\":\"${pushbulletChannel}\"}" \
      --request POST \
      https://api.pushbullet.com/v2/pushes
 ''
