9c5662cf6ff864ec073f8160e151928474f1b19d
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-01-19 23:24:56 -0800

    Call cfg.wrapperProgram properly

--- module.nix
+++ module.nix
@@ -80,8 +80,8 @@ in
         User = "goes-notify";
         Group = "goes-notify";
         DynamicUser = true;
-        ExecStart = ''
-          ${cfg.wrapperProgram} ${goesNotify}/bin/goes-notify --location_id=${cfg.enrollmentLocationId} --interview_date="${cfg.appointmentDate}"
+        ExecStart = cfg.wrapperProgram ''
+          ${goesNotify}/bin/goes-notify --location_id=${cfg.enrollmentLocationId} --interview_date="${cfg.appointmentDate}"
         '';
         Restart = "on-failure";
       };
