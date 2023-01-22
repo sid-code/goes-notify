3fcd89e5760ca4046aa910411ddae5b86ab48549
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-01-22 13:08:14 -0800

    Read pushbullet token from ~/.config

--- notify-script/default.nix
+++ notify-script/default.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  pushbulletToken = pkgs.lib.removeSuffix "\n" (builtins.readFile ./pushbullet-token);
   pushbulletChannel = "goes-notifications-lax";
 in
 pkgs.writeShellScriptBin "notify.sh" ''
-${pkgs.curl}/bin/curl --header 'Access-Token: ${pushbulletToken}' \
+token=$(<$HOME/.config/pushbullet/token)
+${pkgs.curl}/bin/curl --header 'Access-Token: '$token \
      --header 'Content-Type: application/json' \
      --data-binary "{\"body\":\"FOUND $1 BEFORE $2\",\"title\":\"NEW APPT\",\"type\":\"note\",\"channel_tag\":\"${pushbulletChannel}\"}" \
      --request POST \
