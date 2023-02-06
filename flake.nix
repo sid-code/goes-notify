3fe60606da262c6feb7d435b90e65efeeead4110
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-02-05 17:03:42 -0800

    Add dbwrite to flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
       packages.x86_64-linux.default = self.packages.x86_64-linux.goes-notify;
       packages.x86_64-linux.goes-notify = import ./goes-notify.nix { inherit pkgs; };
       packages.x86_64-linux.goes-appts = import ./goes-appts.nix { inherit pkgs; };
+      packages.x86_64-linux.goes-dbwrite = import ./goes-dbwrite.nix { inherit pkgs; };
 
       nixosModules.goes-notify = import ./module.nix { inherit inputs; };
       nixosModules.default = self.nixosModules.goes-notify;
