558a0d82c819d6c179a58ed57991b4a04bbe892f
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-02-05 17:09:55 -0800

    Add experimental goes-check to flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       packages.x86_64-linux.goes-appts = import ./goes-appts.nix { inherit pkgs; };
       packages.x86_64-linux.goes-dbwrite = import ./goes-dbwrite.nix { inherit pkgs; };
 
+      packages.x86_64-linux.goes-check = pkgs.writeShellScriptBin ''
+        ${packages.x86_64-linux.goes-appts} $1 | ${packages.x86_64-linux.goes-dbwrite} $2
+      '';
+
       nixosModules.goes-notify = import ./module.nix { inherit inputs; };
       nixosModules.default = self.nixosModules.goes-notify;
     };
